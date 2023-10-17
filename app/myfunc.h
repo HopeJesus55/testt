de108bfac606ede43b4b44c9479eedb4bd81dace
HopeJesus55 <hopejesus55@gmail.com>
2023-10-17 10:20:56 +0300

    update

--- app/myfunc.h
+++ app/myfunc.h
@@ -1,7 +1,6 @@
 #ifndef MYFUNC_H
 #define MYFUNC_H
 
-int myfunc(int b);
 
 int fibonachi(int num);
 double korni(double a, double b, double c);
--- tests/korni_test.h
+++ tests/korni_test.h
@@ -1,19 +1,18 @@
 #ifndef KORNI_H
 #define KORNI_H
 
-#include <gtest/gtest.h>
-
-#include <fcntl.h>
 #include <errno.h>
-#include <unistd.h>
+#include <fcntl.h>
+#include <gtest/gtest.h>
 #include <math.h>
+#include <unistd.h>
 
 extern "C" {
 #include "myfunc.h"
 }
 
 TEST(korniTest, DPositive) {
-    ASSERT_EQ(korni(1, 2, -3), -2.000000);
+    ASSERT_EQ(korni(1, 2, -3), -2.0L);
     ASSERT_EQ(korni(1, -5, 4), 5.000000);
 }
 
@@ -25,9 +24,9 @@ TEST(korniTest, DNegative) {
     ASSERT_EQ(korni(1, 1, 1), 0);
 }
 
-//Интеграционное
+// Интеграционное
 TEST(korniTest, Integral) {
     ASSERT_EQ(fibonachi(int(korni(1, -4, 4))), 2);
 }
 
-#endif // KORNI_H
+#endif  // KORNI_H
